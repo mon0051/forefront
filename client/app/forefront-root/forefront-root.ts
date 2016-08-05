b24b8a9278782ea539d9cf27e79139aff181437f
andrew.monteith <andrew.monteith@kiandra.com.au>
2016-08-05 11:23:24 +1000

    refactor client and server

--- client/app/forefront-root/forefront-root.ts
+++ client/app/forefront-root/forefront-root.ts
@@ -4,7 +4,6 @@ import {UrlHelper} from '../util/url-helper';
 
 
 import {Component} from '@angular/core';
-import {GameOfLife} from "../game-of-life/game-of-life";
 @Component({
     selector: 'forefront-root',
     templateUrl: UrlHelper.resolvePath('app/forefront-root/forefront-root.html'),
