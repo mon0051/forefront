6bb8a7acb7af93751d2272022b134af3768ed83d
andrew.monteith <andrew.monteith@kiandra.com.au>
2016-10-12 15:56:15 +1100

    remove dotnet core

--- index.ts
+++ index.ts
@@ -1,7 +1,5 @@
 import {startRedbird} from './server/reverseProxy';
 import {startExpress} from "./server/express";
-import {startDotNet} from "./server/dotnet";
 
-//startDotNet();
 startRedbird();
 startExpress();
